5a03f34aa9b322f1546374ec0475dad97c8e9f88
Alanna Smith <smitalan240@gmail.com>
2024-04-25 14:33:31 -0400

    figure out how to make the coo sound play when a tribble hits a wall.

--- Animation.cs
+++ Animation.cs
@@ -2,13 +2,37 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Animation;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
 
 namespace Topic_6_animation_and_classes
 {
     public class Animation : Game
     {
         Texture2D tribbleGreyTexture;
-        Tribble tribble1;
+        Texture2D tribbleCreamTexture;
+        Texture2D tribbleOrangeTexture;
+        Texture2D tribbleBrownTexture;
+
+        List<Texture2D> tribbleTextures;
+
+        Tribble tribble1, tribble2, tribble3, tribble4;
+
+        //SoundEffect coo;
+        //SoundEffectInstance cooInstance;
+
+        Texture2D introscreenTexture;
+
+        private SpriteFont introfont;
+
+        MouseState mouseState;
+
+        enum Screen
+        {
+            Intro,
+            TribbleYard,
+        }
+        Screen screen;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -24,9 +48,21 @@ namespace Topic_6_animation_and_classes
         {
             // TODO: Add your initialization logic here
 
+            screen = Screen.Intro;
+
             base.Initialize();
 
+            
+            tribbleTextures = new List<Texture2D>();
+            tribbleTextures.Add(tribbleGreyTexture);
+            tribbleTextures.Add(tribbleCreamTexture);
+            tribbleTextures.Add(tribbleOrangeTexture);
+            tribbleTextures.Add(tribbleBrownTexture);
+
             tribble1 = new Tribble(tribbleGreyTexture, new Rectangle(10, 10, 100, 100), new Vector2(2, 0));
+            tribble2 = new Tribble(tribbleBrownTexture, new Rectangle(100, 120, 100, 100), new Vector2(2, 2));
+            tribble3 = new Tribble(tribbleCreamTexture, new Rectangle(10, 30, 100, 100));
+            tribble4 = new Tribble(tribbleTextures, new Rectangle(20, 40, 100, 100));
 
         }
 
@@ -34,6 +70,13 @@ namespace Topic_6_animation_and_classes
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             tribbleGreyTexture = Content.Load<Texture2D>("tribbleGrey");
+            tribbleCreamTexture = Content.Load<Texture2D>("tribbleCream");
+            tribbleOrangeTexture = Content.Load<Texture2D>("tribbleOrange");
+            tribbleBrownTexture = Content.Load<Texture2D>("tribbleBrown");
+            introscreenTexture = Content.Load<Texture2D>("introscreen");
+            introfont = Content.Load<SpriteFont>("File");
+            //coo = Content.Load<SoundEffect>("tribble_coo");
+            // cooInstance = coo.CreateInstance();
             // TODO: use this.Content to load your game content here
         }
 
@@ -42,19 +85,45 @@ namespace Topic_6_animation_and_classes
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            tribble1.Move(_graphics);
+            mouseState = Mouse.GetState();
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.TribbleYard;
 
-            // TODO: Add your update logic here
+            }
+            else if (screen == Screen.TribbleYard)
+            {
+                tribble1.Move(_graphics);
+                tribble2.Move(_graphics);
+                tribble3.Move(_graphics);
+                tribble4.Move(_graphics);
+            }
+              
 
-            base.Update(gameTime);
+                // TODO: Add your update logic here
+
+                base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-            _spriteBatch.Draw(tribble1.Texture, tribble1.Bounds, Color.White);
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(introscreenTexture, new Rectangle(0, 0, 800, 500), Color.White);
+                _spriteBatch.DrawString(introfont, ("Left click to see tribbles."), new Vector2(500, 100), Color.White);
+            }
+            else if (screen == Screen.TribbleYard)
+            {
+                _spriteBatch.Draw(tribble1.Texture, tribble1.Bounds, Color.White);
+                _spriteBatch.Draw(tribble2.Texture, tribble2.Bounds, Color.White);
+                _spriteBatch.Draw(tribble3.Texture, tribble3.Bounds, Color.White);
+                _spriteBatch.Draw(tribble4.Texture, tribble4.Bounds, Color.White);
+            }
             _spriteBatch.End();
+               
 
             // TODO: Add your drawing code here
 
--- Tribble.cs
+++ Tribble.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,13 +14,30 @@ namespace Animation
         private Texture2D _texture;
         private Rectangle _rectangle;
         private Vector2 _speed;
+ 
+
+        Random _generator = new Random();
 
         public Tribble(Texture2D texture, Rectangle rect, Vector2 speed)
         {
             _texture = texture;
             _rectangle = rect;
-            _speed = speed;
+            _speed = speed;  
         }
+
+        public Tribble(Texture2D texture, Rectangle rect)
+        {
+            _speed = new Vector2(_generator.Next(1, 10), _generator.Next(1, 10));
+            _texture = texture;
+            _rectangle = rect;
+        }
+
+        public Tribble(List<Texture2D> textures, Rectangle rect)
+        {
+            _speed = new Vector2(_generator.Next(1, 5), _generator.Next(1, 5));
+            _rectangle = rect;
+            _texture = textures[_generator.Next(textures.Count)];
+;        }
 
         public Texture2D Texture
         {
@@ -34,11 +52,18 @@ namespace Animation
 
         public void Move(GraphicsDeviceManager graphics)
         {
+
+
             _rectangle.Offset(_speed);
             if (_rectangle.Right > graphics.PreferredBackBufferWidth || _rectangle.Left < 0)
+            {
                 _speed.X *= -1;
+            }
             if (_rectangle.Bottom > graphics.PreferredBackBufferHeight || _rectangle.Top < 0)
+            {
                 _speed.Y *= -1;
+            }
+                
         }
     }
 }
